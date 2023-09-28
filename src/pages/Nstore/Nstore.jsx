e8404f3cfc1bd0fe7f51e4e0adcd44cbacce40d2
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-09-28 16:41:21 +0200

    Update Nstore.jsx

--- src/pages/Nstore/Nstore.jsx
+++ src/pages/Nstore/Nstore.jsx
@@ -8,20 +8,20 @@ const Nstore = () => {
   return (
     <div>
       <Details
-        buttonHref='/'
-        buttonText='Visit Website'
-        title='NseboStore'
-        description='This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.'
-        tag='Interaction Design / Front End Development'
-        stack='HTML / CSS / JS'
-        projectBackground='This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.'
+        buttonHref="https://teal-smakager-0fcea8.netlify.app/"
+        buttonText="Visit Website"
+        title="Nstore"
+        description="This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes."
+        tag="Interaction Design / Front End Development"
+        stack="HTML / CSS / JS"
+        projectBackground="This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style."
         heroImg={nstoreHero}
         staticImageOne={nstorePreviewOne}
         staticImageTwo={nstorePreviewTwo}
-        nextHref='/Holidaze'
-        nextText='Holidaze'
-        prevHref='/NseboStore'
-        prevText='NseboStore'
+        nextHref="/Holidaze"
+        nextText="Holidaze"
+        prevHref="/NseboStore"
+        prevText="NseboStore"
       />
     </div>
   );
